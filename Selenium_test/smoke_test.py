f2db809c21d347f065929d68ab20e78506e23b25
ArnabKumarBhakta <arnabbhakta5@gmail.com>
2026-09-11 22:52:02 +0530

    Added Test 03 Search + Test 04 Add to Cart

--- Selenium_test/smoke_test.py
+++ Selenium_test/smoke_test.py
@@ -14,6 +14,7 @@ def test_01_website_opens(driver):
     time.sleep(5)
     assert driver.title == "Automation Exercise"
     print("Test 01: Website opens successfully.")
+
 def test_02_user_can_login(driver):
     driver.get("https://automationexercise.com/login")
     time.sleep(3)
@@ -26,5 +27,26 @@ def test_02_user_can_login(driver):
     full_text = driver.find_element(By.XPATH, "//a[contains(text(), 'Logged in as')]").text
     print(f"Test 02: User, {full_text}")
 
+def test_03_search_product(driver):
+    driver.get("https://automationexercise.com/products")
+    time.sleep(3)
+    driver.find_element(By.XPATH, "//input[@id='search_product']").send_keys("Tshirt")
+    driver.find_element(By.XPATH, "//button[@id='submit_search']").click()
+    time.sleep(3)
+
+    result = driver.find_element(By.XPATH, "//h2[@class='title text-center']").text
+    assert "SEARCHED PRODUCTS" in result.upper()
+    print("Test Passed: Products Found - SEARCH TEST PASS")
+
+def test_04_add_to_cart(driver):
+    driver.get("https://automationexercise.com/products")
+    time.sleep(3)
+    driver.find_element(By.XPATH, "(//a[@class='btn btn-default add-to-cart'])[1]").click()
+    time.sleep(3)
+# 2. Click View Cart in popup
+    driver.find_element(By.LINK_TEXT, "View Cart").click()
+    time.sleep(3)
+    print("ADD TO CART TEST PASS")
+    
 if __name__ == "__main__":
     pytest.main(["-v", "-s", __file__])
